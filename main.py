d40d504ff9a061c0db394ee7aae96fc5aed183ab
pokemogukunns <nattyannkunndesu@icloud.com>
2024-11-04 11:35:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,10 @@ def fetch_data_from_invidious(endpoint: str):
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+    
+    @app.get("/home", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
 @app.get("/search", response_class=HTMLResponse)
 async def search(request: Request, q: str):
